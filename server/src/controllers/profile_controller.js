de541eb48703f87fcd85ba906703e32e56eb1aaf
siddhu828 <145180939+siddhu828@users.noreply.github.com>
2025-08-25 12:39:52 +0530

    some more changes

--- server/src/controllers/profile_controller.js
+++ server/src/controllers/profile_controller.js
@@ -1,14 +1,16 @@
 const StudentProfile = require('../models/StudentProfile');
 
 exports.getMyProfile = async (req, res, next) => {
-  try{
+  try {
     const p = await StudentProfile.findOne({ userId: req.user._id });
     res.json(p || null);
-  }catch(err){ next(err); }
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.upsertMyProfile = async (req, res, next) => {
-  try{
+  try {
     const data = req.body;
     data.userId = req.user._id;
     const p = await StudentProfile.findOneAndUpdate(
@@ -17,16 +19,20 @@ exports.upsertMyProfile = async (req, res, next) => {
       { new: true, upsert: true, setDefaultsOnInsert: true }
     );
     res.json(p);
-  }catch(err){ next(err); }
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.deactivateMyProfile = async (req, res, next) => {
-  try{
+  try {
     const p = await StudentProfile.findOneAndUpdate(
       { userId: req.user._id },
       { status: 'deactivated' },
       { new: true }
     );
     res.json(p);
-  }catch(err){ next(err); }
+  } catch (err) {
+    next(err);
+  }
 };
